63a42ad8096e515d40e5ad6dcdf92eb23d4c3575
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-10 23:09:42 -0400

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Jimmy Ghelani on 2023-09-10.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
