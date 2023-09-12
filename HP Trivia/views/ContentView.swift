e1d1522a4a27140ecc5b168b6277d623ded24f66
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-12 08:50:02 -0400

    settings page design

--- HP Trivia/views/ContentView.swift
+++ HP Trivia/views/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var moveBackgroundImage = false
     @State private var animateViewsIn = false
     @State private var showInstructions = false
+    @State private var showSettings = false
     
     var body: some View {
         GeometryReader { geo in
@@ -128,14 +129,17 @@ struct ContentView: View {
                         VStack {
                             if animateViewsIn {
                                 Button {
-                                    // Show settings page
+                                    showSettings = true
                                 } label: {
                                     Image(systemName: "gearshape.fill")
                                         .font(.largeTitle)
                                         .foregroundStyle(.white)
                                         .shadow(radius: 5)
                                 }
-                            .transition(.offset(x: geo.size.width / 4))
+                                .transition(.offset(x: geo.size.width / 4))
+                                .sheet(isPresented: $showSettings, content: {
+                                    Settings()
+                                })
                             }
                         }
                         .animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewsIn)
---
+++ HP Trivia/views/Settings.swift
@@ -0,0 +1,103 @@
+//
+//  Settings.swift
+//  HP Trivia
+//
+//  Created by Jimmy Ghelani on 2023-09-12.
+//
+
+import SwiftUI
+
+enum BookStatus {
+    case active
+    case inactive
+    case locked
+}
+
+struct Settings: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var books: [BookStatus] = [.active, .active, .inactive, .locked, .locked, .locked, .locked]
+    
+    var body: some View {
+        ZStack {
+            InfoBackgroundImage()
+            
+            VStack {
+                Text("Which books would you like to see questions from?")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding(.top)
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(), GridItem()]) {
+                        
+                        ForEach(0..<7) {i in
+                            if books[i] == .active {
+                                ZStack(alignment: .bottomTrailing) {
+                                    Image("hp\(i + 1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                    
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .foregroundStyle(.green)
+                                        .shadow(radius: 1)
+                                        .padding()
+                                }
+                                .onTapGesture {
+                                    books[i] = .inactive
+                                }
+                            }
+                            
+                            else if books[i] == .inactive {
+                                ZStack(alignment: .bottomTrailing) {
+                                    Image("hp\(i + 1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                        .overlay(Rectangle().opacity(0.33))
+                                    
+                                    Image(systemName: "circle")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .foregroundStyle(.green.opacity(0.5))
+                                        .shadow(radius: 1)
+                                        .padding()
+                                }
+                                .onTapGesture {
+                                    books[i] = .active
+                                }
+                            }
+                            
+                            else {
+                                ZStack {
+                                    Image("hp\(i + 1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                        .overlay(Rectangle().opacity(0.75))
+                                    
+                                    Image(systemName: "lock.fill")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .shadow(color: .white.opacity(0.75), radius: 3)
+                                }
+                            }
+                        }
+                    }
+                    .padding()
+                }
+                Button("Done") {
+                    dismiss()
+                }
+                .doneButton()
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    Settings()
+}
